e594bd43d3bc6036d67aec0440f9f3bb9d030fb9
galina.logofatu <galina.logofatu@gmail.com>
2018-12-05 15:54:55 +0200

    + until lesson 24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {User} from './address-card/user.model';
+import {TestService} from './test.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  title = 'first app';
+  user: User;
+  inputText: String = "Initial value";
+
+  constructor(
+      private svc: TestService,
+      private htttp: HttpClient
+  ){
+    this.user = new User();
+    this.user.name = "Foo Bar";
+    this.user.designation = "Software Developer";
+    this.user.address = "1234 Main St, City, State, 100010";
+    this.user.phone = [
+            '123-123-1234',
+            '456-456-4567',
+            '789-789-7890'
+    ]
+
+    this.svc.printToConsole('Got the service!');
+  }
+
+  ngOnInit(){
+    let url = 'https://api.github.com/users/logofatu';
+    let obs = this.htttp.get(url);
+    obs.subscribe((response) => console.log(response));
+
+    // this.htttp.post(URL);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { DateComponent } from './date/date.component';
+import { AddressCardComponent } from './address-card/address-card.component';
+import {FormsModule} from '@angular/forms';
+import {ViewModule} from './view/view.module';
+import {TestService} from './test.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    DateComponent,
+    AddressCardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ViewModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+      TestService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
